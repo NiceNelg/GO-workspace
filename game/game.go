49ceac401891039c34eb22048743789d20b4652d
NiceNelg <472713985@qq.com>
2018-09-23 17:21:35 +0800

    update

--- game/game.go
+++ game/game.go
@@ -16,24 +16,38 @@ type ChessWidget struct {
 	window      *gtk.Window
 	buttonMin   *gtk.Button
 	buttonClose *gtk.Button
-	lableBlack  *gtk.Label
-	lableWhite  *gtk.Label
-	lableTime   *gtk.Label
+	labelBlack  *gtk.Label
+	labelWhite  *gtk.Label
+	labelTime   *gtk.Label
 	imgBlack    *gtk.Image
 	imgWhite    *gtk.Image
 }
 
 //控件属性结构体
 type ChessInfo struct {
-	w, h int
-	x, y int
+	w, h           int
+	x, y           int
+	startX, startY int
+	gridW, gridH   int
 }
 
 //黑白棋结构体
 type Chessboard struct {
 	ChessWidget
 	ChessInfo
+
+	CurrentRole int
+	tipTimeId   int
+
+	chess [8][8]int
 }
+
+//枚举,标识黑白子状态
+const (
+	Empty = iota
+	Black
+	White
+)
 
 func ButtonSetImgFromFile(butto *gtk.Button, filename string) {
 	//获取按钮大小
@@ -50,6 +64,18 @@ func ButtonSetImgFromFile(butto *gtk.Button, filename string) {
 	butto.SetImage(image)
 	//去掉按钮的焦距
 	butto.SetCanFocus(false)
+}
+
+func ImgSetPicFromFile(image *gtk.Image, filename string) {
+	//获取image大小
+	w, h := 0, 0
+	image.GetSizeRequest(&w, &h)
+	//创建pixbuf
+	pixbuf, _ := gdkpixbuf.NewPixbufFromFileAtScale(filename, w-10, h-10, false)
+	//给image设置图片
+	image.SetFromPixbuf(pixbuf)
+	//释放图片
+	pixbuf.Unref()
 }
 
 func (obj *Chessboard) CreateWindow() {
@@ -76,14 +102,31 @@ func (obj *Chessboard) CreateWindow() {
 	ButtonSetImgFromFile(obj.buttonClose, "./images/close.png")
 
 	/*标签相关*/
-	obj.lableBlack = gtk.LabelFromObject(builder.GetObject("lableBlack"))
-	obj.lableWhite = gtk.LabelFromObject(builder.GetObject("lableWhite"))
-	obj.lableTime = gtk.LabelFromObject(builder.GetObject("lableTime"))
+	obj.labelBlack = gtk.LabelFromObject(builder.GetObject("labelBlack"))
+	obj.labelWhite = gtk.LabelFromObject(builder.GetObject("labelWhite"))
+	obj.labelTime = gtk.LabelFromObject(builder.GetObject("labelTime"))
 
 	//设置字体大小
-	obj.lableBlack.ModifyFontSize(50)
-	obj.lableWhite.ModifyFontSize(50)
-	obj.lableTime.ModifyFontSize(50)
+	obj.labelBlack.ModifyFontSize(50)
+	obj.labelWhite.ModifyFontSize(50)
+	obj.labelTime.ModifyFontSize(30)
+
+	obj.labelBlack.SetText("0")
+	obj.labelWhite.SetText("0")
+	obj.labelTime.SetText("20")
+
+	obj.labelBlack.ModifyFG(gtk.STATE_NORMAL, gdk.NewColor("white"))
+	obj.labelWhite.ModifyFG(gtk.STATE_NORMAL, gdk.NewColor("white"))
+	obj.labelTime.ModifyFG(gtk.STATE_NORMAL, gdk.NewColor("white"))
+
+	obj.imgBlack = gtk.ImageFromObject(builder.GetObject("imageBlack"))
+	obj.imgWhite = gtk.ImageFromObject(builder.GetObject("imageWhite"))
+
+	ImgSetPicFromFile(obj.imgBlack, "./images/black.png")
+	ImgSetPicFromFile(obj.imgWhite, "./images/white.png")
+
+	obj.startX, obj.startY = 200, 60
+	obj.gridW, obj.gridH = 50, 40
 }
 
 func MousePressEvent(ctx *glib.CallbackContext) {
@@ -99,6 +142,13 @@ func MousePressEvent(ctx *glib.CallbackContext) {
 	}
 	//保存点击的x,y坐标
 	obj.x, obj.y = int(event.X), int(event.Y)
+	x := (obj.x - obj.startX) / obj.gridW
+	y := (obj.y - obj.startY) / obj.gridH
+
+	if x >= 0 && x <= 7 && y >= 0 && y <= 7 {
+		obj.chess[x][y] = White
+		obj.window.QueueDraw()
+	}
 }
 
 func MouseMoveEvent(ctx *glib.CallbackContext) {
@@ -133,8 +183,25 @@ func PaintEvent(ctx *glib.CallbackContext) {
 	pixbuf, _ := gdkpixbuf.NewPixbufFromFileAtScale("./images/bg.jpg", obj.w, obj.h, false)
 	//画图
 	painter.DrawPixbuf(gc, pixbuf, 0, 0, 0, 0, -1, -1, gdk.RGB_DITHER_NONE, 0, 0)
+
+	//画棋子
+	blackPixbuf, _ := gdkpixbuf.NewPixbufFromFileAtScale("./images/black.png", obj.gridW, obj.gridH, false)
+	whitePixbuf, _ := gdkpixbuf.NewPixbufFromFileAtScale("./images/white.png", obj.gridW, obj.gridH, false)
+
+	for i := 0; i < 8; i++ {
+		for j := 0; j < 8; j++ {
+			if obj.chess[i][j] == Black {
+				painter.DrawPixbuf(gc, blackPixbuf, 0, 0, obj.startX+i*obj.gridW, obj.startY+j*obj.gridH, -1, -1, gdk.RGB_DITHER_NONE, 0, 0)
+			} else if obj.chess[i][j] == White {
+				painter.DrawPixbuf(gc, whitePixbuf, 0, 0, (obj.startX + i*obj.gridW), (obj.startY + j*obj.gridH), -1, -1, gdk.RGB_DITHER_NONE, 0, 0)
+			}
+		}
+	}
+
 	//释放资源
 	pixbuf.Unref()
+	blackPixbuf.Unref()
+	whitePixbuf.Unref()
 }
 
 func (obj *Chessboard) HandleSignal() {
@@ -166,6 +233,37 @@ func (obj *Chessboard) HandleSignal() {
 
 }
 
+func showTip(obj *Chessboard) {
+	if obj.CurrentRole == Black {
+		//隐藏白子
+		obj.imgWhite.Hide()
+		if obj.imgBlack.GetVisible() == true {
+			obj.imgBlack.Hide()
+		} else {
+			obj.imgBlack.Show()
+		}
+	} else {
+		obj.imgBlack.Hide()
+		if obj.imgWhite.GetVisible() == true {
+			obj.imgWhite.Hide()
+		} else {
+			obj.imgWhite.Show()
+		}
+	}
+}
+
+func (obj *Chessboard) InitChess() {
+	obj.imgBlack.Hide()
+	obj.imgWhite.Hide()
+	//黑子先下
+	obj.CurrentRole = Black
+	//启动定时器
+	obj.tipTimeId = glib.TimeoutAdd(500, func() bool {
+		showTip(obj)
+		return true
+	})
+}
+
 func main() {
 	//初始化GTK库
 	gtk.Init(&os.Args)
@@ -174,7 +272,7 @@ func main() {
 	obj.CreateWindow()
 	obj.HandleSignal()
 	obj.window.Show()
-
+	obj.InitChess()
 	//运行主事件
 	gtk.Main()
 }
